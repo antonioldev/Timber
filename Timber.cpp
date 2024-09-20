218f6ad3d9970294bce5bdcbd61143783cd78df5
antonioldev <bauduchi@live.com>
2024-09-20 18:01:33 +0100

    Created energy bar

--- Timber.cpp
+++ Timber.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <iostream>
+#include <sstream>
 #include "SFML/Graphics.hpp"
 
 using namespace sf;
@@ -48,11 +49,142 @@ int main()
     float cloud1Speed = 0.0f;
     float cloud2Speed = 0.0f;
     float cloud3Speed = 0.0f;
-    
+    Clock clock;
+
+    //Timebar
+    RectangleShape timeBar;
+    float timeBarStartWidth = 400;
+	float timeBarHeight = 40;
+	timeBar.setSize(Vector2f(timeBarStartWidth, timeBarHeight));
+	timeBar.setFillColor(Color::Red);
+	timeBar.setPosition((1920 / 2) - timeBarStartWidth / 2, 980);
+
+    RectangleShape timeBarOutline;
+    timeBarOutline.setSize(Vector2f(timeBarStartWidth, timeBarHeight));
+    timeBarOutline.setFillColor(Color::Transparent);
+    timeBarOutline.setOutlineColor(Color::White);
+    timeBarOutline.setOutlineThickness(5);
+    timeBarOutline.setPosition((1920 / 2) - timeBarStartWidth / 2, 980);
+
+
+	Time gameTimeTotal;
+	float timeRemaining = 6.0f;
+	float timeBarWidthPerSecond = timeBarStartWidth / timeRemaining;
+
+    bool paused = true;
+    int score = 0;
+
+    Text messageText;
+    Text scoreText;
+
+    Font font;
+	font.loadFromFile("fonts/KOMIKAP_.ttf");
+	messageText.setFont(font);
+	scoreText.setFont(font);
+
+	messageText.setString("Press Enter to start!");
+    scoreText.setString("SCORE =  0");
+
+	messageText.setCharacterSize(75);
+	scoreText.setCharacterSize(100);
+
+	messageText.setFillColor(Color::White);
+	scoreText.setFillColor(Color::White);
+
+    FloatRect textRect = messageText.getLocalBounds();
+    messageText.setOrigin(textRect.left + textRect.width / 2.0f, textRect.top + textRect.height / 2.0f);
+    messageText.setPosition(1920 / 2.0f, 1080 / 2.0f);
+    scoreText.setPosition(20, 20);
     while (window.isOpen())
     {
         if (Keyboard::isKeyPressed(Keyboard::Escape))
             window.close();
+        if (Keyboard::isKeyPressed(Keyboard::Return))
+        {
+            paused = false;
+			score = 0;
+			timeRemaining = 6;
+        }
+        if (!paused)
+        {
+            Time dt = clock.restart();
+			timeRemaining -= dt.asSeconds();
+			timeBar.setSize(Vector2f(timeBarWidthPerSecond * timeRemaining, timeBarHeight));
+			if (timeRemaining <= 0.0f)
+            {
+                paused = true;
+                messageText.setString("GAME OVER!!");
+				FloatRect textRect = messageText.getLocalBounds();
+				messageText.setOrigin(textRect.left + textRect.width / 2.0f, textRect.top + textRect.height / 2.0f);
+                messageText.setPosition(1920 / 2.0f, 1080 / 2.0f);
+            }
+            if (!beeActive)
+            {
+                srand((int)time(0));
+                beeSpeed = (rand() % 200) + 200;
+                srand((int)time(0) * 10);
+                float height = (rand() % 500) + 500;
+                spriteBee.setPosition(2000, height);
+                beeActive = true;
+            }
+            else
+            {
+                spriteBee.setPosition(spriteBee.getPosition().x - (beeSpeed * dt.asSeconds()), spriteBee.getPosition().y);
+                if (spriteBee.getPosition().x < -100)
+                    beeActive = false;
+            }
+
+            if (!cloud1Active)
+            {
+                srand((int)time(0) * 10);
+                cloud1Speed = (rand() % 200);
+                srand((int)time(0) * 10);
+                float height = (rand() % 150);
+                spriteCloud1.setPosition(-300, height);
+                cloud1Active = true;
+            }
+            else
+            {
+                spriteCloud1.setPosition(spriteCloud1.getPosition().x + (cloud1Speed * dt.asSeconds()), spriteCloud1.getPosition().y);
+                if (spriteCloud1.getPosition().x > 1920)
+                    cloud1Active = false;
+            }
+
+            if (!cloud2Active)
+            {
+                srand((int)time(0) * 20);
+                cloud2Speed = (rand() % 200);
+                srand((int)time(0) * 20);
+                float height = (rand() % 300) - 150;
+                spriteCloud2.setPosition(-300, height);
+                cloud2Active = true;
+            }
+            else
+            {
+                spriteCloud2.setPosition(spriteCloud2.getPosition().x + (cloud2Speed * dt.asSeconds()), spriteCloud2.getPosition().y);
+                if (spriteCloud2.getPosition().x > 1920)
+                    cloud2Active = false;
+            }
+
+            if (!cloud3Active)
+            {
+                srand((int)time(0) * 30);
+                cloud3Speed = (rand() % 200);
+                srand((int)time(0) * 300);
+                float height = (rand() % 450) - 150;
+                spriteCloud3.setPosition(-300, height);
+                cloud3Active = true;
+            }
+            else
+            {
+                spriteCloud3.setPosition(spriteCloud3.getPosition().x + (cloud3Speed * dt.asSeconds()), spriteCloud3.getPosition().y);
+                if (spriteCloud3.getPosition().x > 1920)
+                    cloud3Active = false;
+            }
+        }
+        std::stringstream ss;
+        ss << "SCORE = " << score;
+        scoreText.setString(ss.str());
         window.clear();
         window.draw(spriteBackground);
         window.draw(spriteCloud1);
@@ -60,6 +192,13 @@ int main()
         window.draw(spriteCloud3);
         window.draw(spriteTree);
         window.draw(spriteBee);
+        window.draw(scoreText);
+		window.draw(timeBar);
+        window.draw(timeBarOutline);
+            
+			
+        if (paused)
+            window.draw(messageText);
         window.display();
     }
     return 0;

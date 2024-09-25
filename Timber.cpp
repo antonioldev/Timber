15b20b946332e4d341e8a036f85302af4ffa0010
antonioldev <bauduchi@live.com>
2024-09-25 10:48:06 +0100

    reorganise the code in function and structures

--- Timber.cpp
+++ Timber.cpp
@@ -2,174 +2,84 @@
 #include "Timber.hpp"
 
 using namespace sf;
-const int WIDTH = 1920;
-const int HEIGHT = 1080;
-const int NUM_BRANCHES = 6;
+
 Sprite branches[NUM_BRANCHES];
-enum class side { LEFT, RIGHT, NONE };
 side branchPositions[NUM_BRANCHES];
-
-
 
 int     main()
 {
     VideoMode vm(WIDTH, HEIGHT);
     RenderWindow window(vm , "Timber!!!", Style::Fullscreen);
 
-    Texture textureBackground;
-    textureBackground.loadFromFile("graphics/background.png");
-    Sprite spriteBackground;
-    spriteBackground.setTexture(textureBackground);
-    spriteBackground.setPosition(0, 0);
-    
-    Texture textureTree;
-    textureTree.loadFromFile("graphics/tree.png");
-    Sprite spriteTree;
-    spriteTree.setTexture(textureTree);
-    spriteTree.setPosition(810, 0);
+	t_sound sound;
+	loadSounds(sound);
 
+	t_texture texture;
+    loadTextures(texture);
+
+
+    Sprite spriteBackground;
+    Sprite spriteTree;
     Sprite spriteBGtree1;
     Sprite spriteBGtree2;
     Sprite spriteBGtree3;
-	spriteBGtree1.setTexture(textureTree);
-	spriteBGtree2.setTexture(textureTree);
-	spriteBGtree3.setTexture(textureTree);
-    spriteBGtree1.setPosition(20, -40);
-    spriteBGtree2.scale(0.5, 1);
-    spriteBGtree3.scale(0.4, 1);
-    spriteBGtree2.setPosition(1800, -50);
-    spriteBGtree3.setPosition(1600, -70);
+    Sprite spritePlayer;
+    Sprite spriteGravestone;
+    Sprite spriteAxe;
+    Sprite spriteLog;
+    t_bg_obj bee;
+    t_bg_obj clouds[NUM_CLOUDS];
 
-    Texture textureBee;
-    textureBee.loadFromFile("graphics/bee.png");
-    Sprite spriteBee;
-    spriteBee.setTexture(textureBee);
-    spriteBee.setPosition(-100, 800);
-    bool beeActive = false;
-    float beeSpeed = 0.0f;
+	loadSprites(spriteBackground, texture.background, 0, 0, 1, 1);  
+	loadSprites(spriteTree, texture.tree, 810, 0, 1, 1);
+	loadSprites(spriteBGtree1, texture.tree, 20, -40, 1, 1);
+	loadSprites(spriteBGtree2, texture.tree, 1800, -50, 0.5, 1);
+	loadSprites(spriteBGtree3, texture.tree, 1600, -70, 0.4, 1);
+	loadSprites(bee.sprite, texture.bee, -100, 800, 1, 1);
+    for (int i = 0; i < NUM_CLOUDS; i++)
+        loadSprites(clouds[i].sprite, texture.cloud, -1000, 0 * 250, 1, 1);
 
-    Texture textureCloud;
-    textureCloud.loadFromFile("graphics/cloud.png");
-    Sprite spriteCloud1;
-    Sprite spriteCloud2;
-    Sprite spriteCloud3;
-    spriteCloud1.setTexture(textureCloud);
-    spriteCloud2.setTexture(textureCloud);
-    spriteCloud3.setTexture(textureCloud);
-    spriteCloud1.setPosition(-1000, 0);
-    spriteCloud2.setPosition(-1000, 250);
-    spriteCloud3.setPosition(-1000, 500);
-    bool cloud1Active = false;
-    bool cloud2Active = false;
-    bool cloud3Active = false;
-    float cloud1Speed = 0.0f;
-    float cloud2Speed = 0.0f;
-    float cloud3Speed = 0.0f;
-    Clock clock;
-
-    //Timebar
-    RectangleShape timeBar;
-    float timeBarStartWidth = 400;
-	float timeBarHeight = 40;
-	timeBar.setSize(Vector2f(timeBarStartWidth, timeBarHeight));
-	timeBar.setFillColor(Color::Red);
-	timeBar.setPosition((WIDTH / 2.0f) - timeBarStartWidth / 2, 980);
-
-    RectangleShape timeBarOutline;
-    timeBarOutline.setSize(Vector2f(timeBarStartWidth, timeBarHeight));
-    timeBarOutline.setFillColor(Color::Transparent);
-    timeBarOutline.setOutlineColor(Color::White);
-    timeBarOutline.setOutlineThickness(5);
-    timeBarOutline.setPosition((WIDTH / 2.0f) - timeBarStartWidth / 2, 980);
-
-
-	Time gameTimeTotal;
-	float timeRemaining = 6.0f;
-	float timeBarWidthPerSecond = timeBarStartWidth / timeRemaining;
-
-    bool paused = true;
-    int score = 0;
-
-    Text messageText;
-    Text scoreText;
-
-    Font font;
-	font.loadFromFile("fonts/KOMIKAP_.ttf");
-	messageText.setFont(font);
-	scoreText.setFont(font);
-
-	messageText.setString("Press Enter to start!");
-    scoreText.setString("SCORE =  0");
-
-	messageText.setCharacterSize(75);
-	scoreText.setCharacterSize(100);
-
-	messageText.setFillColor(Color::White);
-	scoreText.setFillColor(Color::White);
-
-    FloatRect textRect = messageText.getLocalBounds();
-    messageText.setOrigin(textRect.left + textRect.width / 2.0f, textRect.top + textRect.height / 2.0f);
-    messageText.setPosition(WIDTH / 2.0f, 1080 / 2.0f);
-    scoreText.setPosition(20, 20);
-
-
-    Texture textureBranch;
-    textureBranch.loadFromFile("graphics/branch.png");
     for (int i = 0; i < NUM_BRANCHES; i++)
     {
-        branches[i].setTexture(textureBranch);
-        branches[i].setPosition(-2000, -2000);
+		loadSprites(branches[i], texture.branch, -2000, -2000, 1, 1);
         branches[i].setOrigin(220, 20);
     }
-
-    Texture texturePlayer;
-    texturePlayer.loadFromFile("graphics/player.png");
-    Sprite spritePlayer;
-    spritePlayer.setTexture(texturePlayer);
-    spritePlayer.setPosition(580, 720);
+	loadSprites(spritePlayer, texture.player, 580, 720, 1, 1);
     side playerSide = side::LEFT;
+	loadSprites(spriteGravestone, texture.gravestone, 675, 2000, 1, 1);
+	loadSprites(spriteAxe, texture.axe, 700, 830, 1, 1);
+	loadSprites(spriteLog, texture.log, 810, 720, 1, 1);
 
-    Texture textureGravestone;
-	textureGravestone.loadFromFile("graphics/rip.png");
-	Sprite spriteGravestone;
-	spriteGravestone.setTexture(textureGravestone);
-    spriteGravestone.setPosition(675, 2000);
-	//spriteGravestone.setPosition(600, 860);
-
-	Texture textureAxe;
-	textureAxe.loadFromFile("graphics/axe.png");
-	Sprite spriteAxe;
-	spriteAxe.setTexture(textureAxe);
-	spriteAxe.setPosition(700, 830);
-
-	const float AXE_POSITION_LEFT = 700;
-	const float AXE_POSITION_RIGHT = 1075;
-
-	Texture textureLog;
-    textureLog.loadFromFile("graphics/log.png");
-    Sprite spriteLog;
-    spriteLog.setTexture(textureLog);
-    spriteLog.setPosition(810, 720);
 
     bool logActive = false;
     float logSpeedX = 1000;
     float logSpeedY = -1500;
     bool acceptInput = false;
+    bool paused = true;
+    int score = 0;
 
-    SoundBuffer chopBuffer;
-	chopBuffer.loadFromFile("sound/chop.wav");
-	Sound chop;
-	chop.setBuffer(chopBuffer);
 
-    SoundBuffer deathBuffer;
-    deathBuffer.loadFromFile("sound/death.wav");
-    Sound death;
-    death.setBuffer(deathBuffer);
 
-    SoundBuffer ootBuffer;
-    ootBuffer.loadFromFile("sound/out_of_time.wav");
-    Sound oot;
-    oot.setBuffer(ootBuffer);
+    Clock clock;
+
+
+	t_timebar timeBar;
+    set_timeBar(timeBar);
+
+
+	Time gameTimeTotal;
+	float timeRemaining = 6.0f;
+	float timeBarWidthPerSecond = timeBar.startWidth / timeRemaining;
+
+	t_text text;
+	set_text(text);
+    FloatRect textRect = text.message.getLocalBounds();
+    text.message.setOrigin(textRect.left + textRect.width / 2.0f, textRect.top + textRect.height / 2.0f);
+    text.message.setPosition(WIDTH / 2.0f, HEIGHT / 2.0f);
+    text.score.setPosition(20, 20);
+
+
+    
 
     while (window.isOpen())
     {
@@ -196,14 +106,14 @@ int     main()
                 timeRemaining += (2.0 / score) + 0.15;
 				if (timeRemaining > 6.0f)
 					timeRemaining = 6.0f;
-                spriteAxe.setPosition(AXE_POSITION_RIGHT, spriteAxe.getPosition().y);
+                spriteAxe.setPosition((float)AXE_POSITION_RIGHT, spriteAxe.getPosition().y);
                 spritePlayer.setPosition(1200, 720);
                 updateBranches(score);
                 spriteLog.setPosition(810, 720);
                 logSpeedX = -5000;
                 logActive = true;
                 acceptInput = false;
-                chop.play();
+                sound.chop.play();
             }
             if (Keyboard::isKeyPressed(Keyboard::Left))
             {
@@ -212,14 +122,14 @@ int     main()
                 timeRemaining += (2.0 / score) + 0.15;
                 if (timeRemaining > 6.0f)
                     timeRemaining = 6.0f;
-                spriteAxe.setPosition(AXE_POSITION_LEFT, spriteAxe.getPosition().y);
+                spriteAxe.setPosition((float)AXE_POSITION_LEFT, spriteAxe.getPosition().y);
                 spritePlayer.setPosition(580, 720);
                 updateBranches(score);
                 spriteLog.setPosition(810, 720);
                 logSpeedX = 5000;
                 logActive = true;
                 acceptInput = false;
-                chop.play();
+                sound.chop.play();
             }
         }
 		Event event;
@@ -236,82 +146,54 @@ int     main()
         {
             Time dt = clock.restart();
             timeRemaining -= dt.asSeconds();
-            timeBar.setSize(Vector2f(timeBarWidthPerSecond * timeRemaining, timeBarHeight));
+            timeBar.timeBar.setSize(Vector2f(timeBarWidthPerSecond * timeRemaining, timeBar.height));
             if (timeRemaining <= 0.0f)
             {
-				oot.play();
+                sound.oot.play();
                 paused = true;
-                messageText.setString("GAME OVER!!");
-                FloatRect textRect = messageText.getLocalBounds();
-                messageText.setOrigin(textRect.left + textRect.width / 2.0f, textRect.top + textRect.height / 2.0f);
-                messageText.setPosition(WIDTH / 2.0f, HEIGHT / 2.0f);
+                text.message.setString("GAME OVER!!");
+                FloatRect textRect = text.message.getLocalBounds();
+                text.message.setOrigin(textRect.left + textRect.width / 2.0f, textRect.top + textRect.height / 2.0f);
+                text.message.setPosition(WIDTH / 2.0f, HEIGHT / 2.0f);
             }
-            if (!beeActive)
+            if (!bee.active)
             {
                 srand((int)time(0));
-                beeSpeed = (rand() % 200) + 200;
+                bee.speed = (rand() % 200) + 200;
                 srand((int)time(0) * 10);
                 float height = (rand() % 500) + 500;
-                spriteBee.setPosition(2000, height);
-                beeActive = true;
+                bee.sprite.setPosition(2000, height);
+                bee.active = true;
             }
             else
             {
-                spriteBee.setPosition(spriteBee.getPosition().x - (beeSpeed * dt.asSeconds()), spriteBee.getPosition().y);
-                if (spriteBee.getPosition().x < -100)
-                    beeActive = false;
+                // add movement up and down bee
+                bee.sprite.setPosition(bee.sprite.getPosition().x - (bee.speed * dt.asSeconds()), bee.sprite.getPosition().y);
+                if (bee.sprite.getPosition().x < -100)
+                    bee.active = false;
+            }
+            for (int i = 0; i < NUM_CLOUDS; i++)
+            {
+                if (!clouds[i].active)
+                {
+                    srand((int)time(0) * 10 * i);
+                    clouds[i].speed = (rand() % 200);
+                    srand((int)time(0) * 10 * i);
+                    float height = (rand() % 150);
+                    clouds[i].sprite.setPosition(-300, height);
+                    clouds[i].active = true;
+                }
+                else
+                {
+                    clouds[i].sprite.setPosition(clouds[i].sprite.getPosition().x + (clouds[i].speed * dt.asSeconds()), clouds[i].sprite.getPosition().y);
+                    if (clouds[i].sprite.getPosition().x > WIDTH)
+                        clouds[i].active = false;
+                }
             }
 
-            if (!cloud1Active)
-            {
-                srand((int)time(0) * 10);
-                cloud1Speed = (rand() % 200);
-                srand((int)time(0) * 10);
-                float height = (rand() % 150);
-                spriteCloud1.setPosition(-300, height);
-                cloud1Active = true;
-            }
-            else
-            {
-                spriteCloud1.setPosition(spriteCloud1.getPosition().x + (cloud1Speed * dt.asSeconds()), spriteCloud1.getPosition().y);
-                if (spriteCloud1.getPosition().x > WIDTH)
-                    cloud1Active = false;
-            }
-
-            if (!cloud2Active)
-            {
-                srand((int)time(0) * 20);
-                cloud2Speed = (rand() % 200);
-                srand((int)time(0) * 20);
-                float height = (rand() % 300) - 150;
-                spriteCloud2.setPosition(-300, height);
-                cloud2Active = true;
-            }
-            else
-            {
-                spriteCloud2.setPosition(spriteCloud2.getPosition().x + (cloud2Speed * dt.asSeconds()), spriteCloud2.getPosition().y);
-                if (spriteCloud2.getPosition().x > WIDTH)
-                    cloud2Active = false;
-            }
-
-            if (!cloud3Active)
-            {
-                srand((int)time(0) * 30);
-                cloud3Speed = (rand() % 200);
-                srand((int)time(0) * 300);
-                float height = (rand() % 450) - 150;
-                spriteCloud3.setPosition(-300, height);
-                cloud3Active = true;
-            }
-            else
-            {
-                spriteCloud3.setPosition(spriteCloud3.getPosition().x + (cloud3Speed * dt.asSeconds()), spriteCloud3.getPosition().y);
-                if (spriteCloud3.getPosition().x > WIDTH)
-                    cloud3Active = false;
-            }
             std::stringstream ss;
             ss << "SCORE = " << score;
-            scoreText.setString(ss.str());
+            text.score.setString(ss.str());
             for (int i = 0; i < NUM_BRANCHES; i++)
             {
                 float height = i * 150;
@@ -343,17 +225,17 @@ int     main()
 
             if (branchPositions[5] == playerSide)
             {
-                death.play();
+                sound.death.play();
                 paused = true;
                 acceptInput = false;
                 spriteGravestone.setPosition(525, 760);
 				spriteAxe.setPosition(2000, 830);
 				spriteLog.setPosition(2000, 720);
 				spritePlayer.setPosition(2000, 660);
-				messageText.setString("SQUISHED!!");
-				FloatRect textRect = messageText.getLocalBounds();
-                messageText.setOrigin(textRect.left + textRect.width / 2.0f, textRect.top + textRect.height / 2.0f);
-                messageText.setPosition(1920 / 2.0f, 1080 / 2.0f);
+                text.message.setString("SQUISHED!!");
+				FloatRect textRect = text.message.getLocalBounds();
+                text.message.setOrigin(textRect.left + textRect.width / 2.0f, textRect.top + textRect.height / 2.0f);
+                text.message.setPosition(WIDTH / 2.0f, HEIGHT / 2.0f);
             }
         }
 
@@ -363,25 +245,23 @@ int     main()
 		window.draw(spriteBGtree1);
 		window.draw(spriteBGtree2);
 		window.draw(spriteBGtree3);
-        window.draw(spriteCloud1);
-        window.draw(spriteCloud2);
-        window.draw(spriteCloud3);
+		for (int i = 0; i < NUM_CLOUDS; i++)
+			window.draw(clouds[i].sprite);
         for (int i = 0; i < NUM_BRANCHES; i++)
             window.draw(branches[i]);
-        /*window.draw(spriteLog);*/
         window.draw(spriteTree);
         window.draw(spritePlayer);
         window.draw(spriteAxe);
         window.draw(spriteLog);
         window.draw(spriteGravestone);
-        window.draw(spriteBee);
-        window.draw(scoreText);
-		window.draw(timeBar);
-        window.draw(timeBarOutline);
+        window.draw(bee.sprite);
+        window.draw(text.score);
+		window.draw(timeBar.timeBar);
+        window.draw(timeBar.outline);
             
 			
         if (paused)
-            window.draw(messageText);
+            window.draw(text.message);
         window.display();
     }
     return 0;
@@ -407,3 +287,64 @@ void    updateBranches(int seed)
     }
 }
 
+void loadSounds(t_sound& sound)
+{
+	sound.chopBuffer.loadFromFile("sound/chop.wav");
+	sound.deathBuffer.loadFromFile("sound/death.wav");
+	sound.ootBuffer.loadFromFile("sound/out_of_time.wav");
+	sound.chop.setBuffer(sound.chopBuffer);
+	sound.death.setBuffer(sound.deathBuffer);
+	sound.oot.setBuffer(sound.ootBuffer);
+}
+
+void	loadTextures(t_texture& texture)
+{
+	texture.background.loadFromFile("graphics/background.png");
+	texture.tree.loadFromFile("graphics/tree.png");
+	texture.bee.loadFromFile("graphics/bee.png");
+	texture.cloud.loadFromFile("graphics/cloud.png");
+	texture.branch.loadFromFile("graphics/branch.png");
+	texture.player.loadFromFile("graphics/player.png");
+	texture.gravestone.loadFromFile("graphics/rip.png");
+	texture.axe.loadFromFile("graphics/axe.png");
+	texture.log.loadFromFile("graphics/log.png");
+}
+
+void	loadSprites(sf::Sprite& sprite, sf::Texture& texture, int posX, int posY, float scaleX, float scaleY)
+{
+    sprite.setTexture(texture);
+    sprite.scale(scaleX, scaleY);
+    sprite.setPosition(posX, posY);
+}
+
+void	set_timeBar(t_timebar& timeBar)
+{
+    timeBar.startWidth = 400;
+    timeBar.height = 40;
+    timeBar.timeBar.setSize(Vector2f(timeBar.startWidth, timeBar.height));
+    timeBar.timeBar.setFillColor(Color::Red);
+    timeBar.timeBar.setPosition((WIDTH / 2.0f) - timeBar.startWidth / 2, 980);
+
+
+    timeBar.outline.setSize(Vector2f(timeBar.startWidth, timeBar.height));
+    timeBar.outline.setFillColor(Color::Transparent);
+    timeBar.outline.setOutlineColor(Color::White);
+    timeBar.outline.setOutlineThickness(5);
+    timeBar.outline.setPosition((WIDTH / 2.0f) - timeBar.startWidth / 2, 980);
+}
+
+void	set_text(t_text& text)
+{
+    text.font.loadFromFile("fonts/KOMIKAP_.ttf");
+    text.message.setFont(text.font);
+    text.score.setFont(text.font);
+
+    text.message.setString("Press Enter to start!");
+    text.score.setString("SCORE =  0");
+
+    text.message.setCharacterSize(75);
+    text.score.setCharacterSize(100);
+
+    text.message.setFillColor(Color::White);
+    text.score.setFillColor(Color::White);
+}
--- Timber.hpp
+++ Timber.hpp
@@ -5,18 +5,82 @@
 #include "SFML/Graphics.hpp"
 #include "SFML/Audio.hpp"
 
-void    updateBranches(int seed);
+#define WIDTH 1920
+#define HEIGHT 1080
+#define NUM_BRANCHES 6
+#define NUM_CLOUDS 3
+#define NUM_TREE 3
 
-//typedef struct s_texture
+const float AXE_POSITION_LEFT = 700;
+const float AXE_POSITION_RIGHT = 1075;
+
+enum class side { LEFT, RIGHT, NONE };
+
+typedef struct s_texture
+{
+	sf::Texture background;
+	sf::Texture tree;
+	sf::Texture bee;
+	sf::Texture cloud;
+	sf::Texture branch;
+	sf::Texture player;
+	sf::Texture gravestone;
+	sf::Texture axe;
+	sf::Texture log;
+
+} t_texture;
+
+typedef struct s_sound
+{
+	sf::SoundBuffer chopBuffer;
+	sf::SoundBuffer deathBuffer;
+	sf::SoundBuffer ootBuffer;
+	sf::Sound chop;
+	sf::Sound death;
+	sf::Sound oot;
+} t_sound;
+
+typedef struct s_bg_obj
+{
+	sf::Sprite sprite;
+	float speed = 0.0f;
+	bool active = false;
+} t_bg_obj;
+
+typedef struct s_timebar
+{
+	sf::RectangleShape timeBar;
+	sf::RectangleShape outline;
+	float startWidth;
+	float height;
+} t_timebar;
+
+typedef struct s_text
+{
+	sf::Font font;
+	sf::Text message;
+	sf::Text score;
+} t_text;
+
+//typedef struct s_sprite
 //{
-//	Texture textureBackground;
-//	Texture textureTree;
-//	Texture textureBee;
-//	Texture textureCloud;
-//	Texture textureBranch;
-//	Texture texturePlayer;
-//	Texture textureRIP;
-//	Texture textureAxe;
-//	Texture textureLog;
-//
-//} t_texture;
+//	Sprite spriteBackground;
+//	Sprite spriteTree;
+//	Sprite spriteBGtree1;
+//	Sprite spriteBGtree2;
+//	Sprite spriteBGtree3;
+//	Sprite spritePlayer;
+//	Sprite spriteGravestone;
+//	Sprite spriteAxe;
+//	Sprite spriteLog;
+//	t_bg_obj bee;
+//	t_bg_obj clouds[NUM_CLOUDS];
+//} t_sprite;
+
+
+void    updateBranches(int seed);
+void	loadSounds(t_sound& sound);
+void	loadTextures(t_texture& texture);
+void	loadSprites(sf::Sprite& sprite, sf::Texture& texture, int posX, int posY, float scaleX, float scaleY);
+void	set_timeBar(t_timebar& timeBar);
+void	set_text(t_text& text);
